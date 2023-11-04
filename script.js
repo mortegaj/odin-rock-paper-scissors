b61c823d0463ba1ddc0566a81da3d1e4c9d69ae7
mortegaj <marcmordigan@gmail.com>
2023-11-04 20:08:55 +0100

    Add function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let randomNumber = Math.random()*100;
+    let computerChoice;
+    
+    if (randomNumber < 33) computerChoice = "rock";
+    else if (randomNumber < 66) computerChoice = "paper";
+    else computerChoice = "scissors";
+    return computerChoice
+}
+
+console.log(getComputerChoice());
