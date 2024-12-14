2b01f66e0760e7a9e041f073cae8d30076ae9e9d
Marc Ortega <marcmordigan@gmail.com>
2024-12-14 14:12:26 +0100

    Refactor using const

--- javascript.js
+++ javascript.js
@@ -1,57 +1,77 @@
-const choiceList = ['PEDRA' , 'PAPER', 'TISORES']
+
+const ROCK = "PEDRA";
+const PAPER = "PAPER";
+const SCISSORS = "TISORES";
+const QUIT = "SORTIR"
+const ERROR = "error"
+
 let humanScore = 0;
 let computerScore = 0;
 
+let computerChoice;
+let humanChoice;
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    return choiceList[randomNumber];
+    if (randomNumber == 0) return ROCK;
+    if (randomNumber == 1) return PAPER;
+    if (randomNumber == 2) return SCISSORS;
+    return ERROR;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt('Escull una opció: (1)PEDRA, (2)PAPER, (3)TISORES or (0)Sortir')
-    if (humanChoice.toLowerCase() == '1') return choiceList[0];
-    if (humanChoice.toLowerCase() == '2') return choiceList[1];
-    if (humanChoice.toLowerCase() == '3') return choiceList[2];
-    if (humanChoice.toLowerCase() == '0') return 'SORTIR';
-    return 'Error'
+    let humanChoice = prompt('Escull una opció: (1)' + ROCK + ', (2)' + PAPER + ', (3)' + SCISSORS + ' or (0)' + QUIT);
+    if (humanChoice == 1) return ROCK;
+    if (humanChoice == 2) return PAPER;
+    if (humanChoice == 3) return SCISSORS;
+    if (humanChoice == 0) return QUIT;
+    return ERROR;
 }
 
 function playRound(humanChoice, computerChoice) {
-    let humanChoiceUpperCase = humanChoice.toUpperCase();
-    let computerChoiceUpperCase = computerChoice.toUpperCase();
-    let winner = 'TIE';
+    
+    let winner = null;
 
-    if (humanChoiceUpperCase == "PEDRA") {
-        if (computerChoiceUpperCase == "PEDRA") {
+    if (humanChoice == ROCK) {
+        if (computerChoice == ROCK) {
             winner = "TIE";
-        } else if (computerChoiceUpperCase == "PAPER") {
-            winner = 'COMPUTER';
-        } else {
-            winner = 'HUMAN';
-        }
-    } else if (humanChoiceUpperCase == "PAPER") {
-        if (computerChoiceUpperCase == "PEDRA") {
-            winner = "HUMAN";
-        } else if (computerChoiceUpperCase == "PAPER") {
-            winner = "TIE";
-        } else {
+        } else if (computerChoice == PAPER) {
             winner = "COMPUTER";
+        } else if (computerChoice == SCISSORS) {
+            winner = "HUMAN";
+        } else {
+            winner == "ERROR";
+        }
+    } else if (humanChoice == PAPER) {
+        if (computerChoice == ROCK) {
+            winner = "HUMAN";
+        } else if (computerChoice == PAPER) {
+            winner = "TIE";
+        } else if (computerChoice == SCISSORS) {
+            winner = "COMPUTER";
+        } else {
+            winner == "ERROR";
+        }
+    } else if (humanChoice == SCISSORS) {
+        if (computerChoice == ROCK) {
+            winner = "COMPUTER";
+        } else if (computerChoice == PAPER) {
+            winner = "HUMAN";
+        } else if (computerChoice == SCISSORS) {
+            winner = "TIE";
+        } else {
+            winner == "ERROR";
         }
     } else {
-        if (computerChoiceUpperCase == "PEDRA") {
-            winner = "COMPUTER";
-        } else if (computerChoiceUpperCase == "PAPER") {
-            winner = "HUMAN";
-        } else {
-            winner = "TIE";
-        }
+        winner = "ERROR"
     }
 
    if (winner == "HUMAN") {
-        console.log("Felicitats, has guanyat");
+        console.log("Felicitats! Has guanyat");
         humanScore += 1;
    } else if (winner  == "COMPUTER") {
-        console.log("Mala sort, has perdut, torna-ho a intentat");
+        console.log("Mala sort, has perdut! Torna-ho a intentat");
         computerScore += 1;
    } else {
         console.log("Heu empatat!");
@@ -60,14 +80,17 @@ function playRound(humanChoice, computerChoice) {
 }
 
 console.log("--------------------")
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-if (choiceList.includes(humanChoice)) {
+
+computerChoice = getComputerChoice();
+humanChoice = getHumanChoice();
+
+if ((humanChoice == ROCK) || (humanChoice == PAPER) || (humanChoice == SCISSORS)) {
     console.log ("Has escollit " + humanChoice + " | l'ordinador ha escollit " + computerChoice);
     playRound(humanChoice,computerChoice);
-} else if (humanChoice == "SORTIR") {
+} else if (humanChoice == QUIT) {
     console.log("Has escollit sortir");
 } else {
     console.log("L'opció escollida no es correcta")
 }
-console.log("--------------------")
+
+console.log("--------------------");
