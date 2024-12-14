44ffcafe625cb857b76553bfcd3641c04e5b1706
Marc Ortega <marcmordigan@gmail.com>
2024-12-14 10:22:16 +0100

    get computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choiceList = ['Rock' , 'Paper', 'Scissors']
+    let randomNumber = Math.floor(Math.random() * 3);
+    return choiceList[randomNumber];
+}
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
