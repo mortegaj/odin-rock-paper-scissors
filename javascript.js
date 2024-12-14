20c1cc0e4436cafafa84bfc6e1e643746d6cb430
Marc Ortega <marcmordigan@gmail.com>
2024-12-14 10:35:59 +0100

    get human choice

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
+const choiceList = ['Rock' , 'Paper', 'Scissors']
+
 function getComputerChoice() {
-    const choiceList = ['Rock' , 'Paper', 'Scissors']
     let randomNumber = Math.floor(Math.random() * 3);
     return choiceList[randomNumber];
 }
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+function getHumanChoice() {
+    let humanChoice = prompt('Your choice: (r)ock, (p)aper, (s)cissors or (q)uit')
+    if (humanChoice.toLowerCase() == 'r') return choiceList[0];
+    if (humanChoice.toLowerCase() == 'p') return choiceList[1];
+    if (humanChoice.toLowerCase() == 's') return choiceList[2];
+    if (humanChoice.toLowerCase() == 'q') return 'Quit';
+    return 'Error'
+}
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+console.log('Computer Choice: ' + computerChoice + ' | Human Choice: ' + humanChoice);
